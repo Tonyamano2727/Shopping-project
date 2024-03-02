c315b5b35739f9f91cfb623e08235f33ba0b3f96
Tonyamano2727 <toanb3074@gmail.com>
2024-03-02 13:54:39 +0700

    creat brand

---
+++ data/cate_brand.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        cate: 'Smartphone',
+        brand: ['Apple', 'Samsung','LG','Asus','Nokia','BlackBerry']
+    },
+    {
+        cate: 'Tablet',
+        brand: ['Ipad', 'SamSung','Acer','Asus','Lenovo','Haier']
+    },
+    {
+        cate: 'Laptop',
+        brand: ['Asus', 'Dell','Acer','Lenovo','HP','Macbook']
+    },
+    {
+        cate: 'Accessories',
+        brand: ['Smat', 'Headphone','Bluetooth','Keyboard','Mouse','Cases']
+    },
+    {
+        cate: 'Television',
+        brand: ['Apple', 'Samsung','LG','Asus','Nokia','BlackBerry']
+    },
+    {
+        cate: 'Printer',
+        brand: ['Samsung', 'Acer','Asus','Lenovo','Haier']
+    }
+]
