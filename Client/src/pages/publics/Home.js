b43a5dfb83c01f0ed22ce2ac9a5c2d10e30a438e
Tonyamano2727 <toanb3074@gmail.com>
2024-03-05 00:12:53 +0700

    create folder and component client side

---
+++ Client/src/pages/publics/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header,Banner,Navigation,Sidebar } from '../../components'
+const Home = () => {
+  return (
+    <div className='w-main'>
+      Home
+    </div>
+  )
+}
+
+export default Home
